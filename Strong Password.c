b3e72bd3068fa57044600cc036399d44c5170f55
vaishnavi-2509 <agrawalvaishnavi2905@gmail.com>
2024-04-10 23:31:16 +0530

    Strong Password

---
+++ Strong Password.c
@@ -0,0 +1,189 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+int minimumNumber(int n, char* password) {
+int count1=0,count2=0,count3=0,count4=0;
+
+ for(int i=0;i<n;i++)
+ {   
+        if(password[i]>='0' && password[i]<='9')
+        {
+            count1=1;
+        }
+        else if(password[i]>='a' && password[i]<='z')
+        {
+            count2=1;
+        }
+        else if(password[i]>='A' && password[i]<='Z')
+        {
+             count3=1;
+        }
+        else {
+         count4=1;
+        }
+ }
+  int count=0;
+ if(count1==0)
+ count++;
+ if(count2==0)
+ count++;
+ if(count3==0)
+ count++;
+ if(count4==0)
+ count++;
+ 
+ if(n<6)
+ {
+     if(6-n>count)
+     {
+         return 6-n;
+     }
+     else {
+ return count;
+ }
+ }
+ else
+ {
+     return count;
+ }
+ return NULL;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char* password = readline();
+
+    int answer = minimumNumber(n, password);
+
+    fprintf(fptr, "%d\n", answer);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
